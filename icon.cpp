4566b703963d03958a61a04ae7f18c99471b8c01
michaellian0422 <71887167+michaellian0422@users.noreply.github.com>
2020-11-17 20:56:51 +0800

    Add files via upload

---
+++ icon.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(){
+	cout << " /////////////\\\\\\\\\ \n";
+	cout << " (((((((((((((( \\\\\\\ \n";
+	cout << " ))) ~~      ~~  ((( \n";
+	cout << " ((( (*)     (*) ))) \n";
+	cout << " )))     <       ((( \n";
+	cout << " ((( '\\______/`  ))) \n";
+	cout << " )))\\___________/(((  \n";
+	cout << "        _) (_  \n";
+	cout << "       / \\_/ \\  \n";
+	cout << "      /(     )\\  \n";
+	cout << "     // )___( \\\\  \n";
+	cout << "     \\\\(     )// \n";
+	cout << "      (       ) \n";
+	cout << "       |  |  | \n";
+	cout << "        | | | \n";
+	cout << "        | | | \n";
+	cout << "       _|_|_|_ \n";
+
+	cout << endl;
+
+	cout << "      ////^\\\\\\ \n";
+	cout << "      | ^   ^ | \n";
+	cout << "     @ (o) (o) @ \n";
+	cout << "      |   <   | \n";
+	cout << "      |  ___  | \n";
+	cout << "       \\_____/ \n";
+	cout << "     ____|  |____ \n";
+	cout << "    /    \\__/    \\ \n";
+	cout << "   /              \\ \n";
+	cout << "  /\\_/|        |\\_/\\ \n";
+	cout << " / /  |        |  \\ \\ \n";
+	cout << "( <   |        |   > ) \n";
+	cout << " \\ \\  |        |  / / \n";
+	cout << "  \\ \\ |________| / / \n";
+
+	cout << endl;
+
+	cout << "    \\\\\\|||/// \n";
+	cout << "  .  =======  \n";
+	cout << " / \\| O   O | \n";
+	cout << " \\ / \\`___'/  \n";
+	cout << " (#) (     )  \n";
+	cout << "  #\\//|   |\\\\  \n";
+	cout << "  #\\/(  *  )/ \n";
+	cout << "  #   =====   \n";
+	cout << "  #   (   )  \n";
+	cout << "  #   || || \n";
+	cout << "  #   || || \n";
+	cout << " .#---'| |`----. \n";
+	cout << " `#----' `-----' \n";
+
+	cout << endl; 
+
+	cout << "        _______" << endl;
+	cout << "       //  ||\\ \\" << endl;
+	cout << " _____//___||_\\ \\___" << endl;
+	cout << " )  _          _    \\" << endl;
+	cout << " |_/ \\________/ \\___|" << endl;
+	cout << "___\\_/________\\_/______" << endl;
+
+	cout << endl;
+
+	cout << "          .:'" << endl;
+	cout << "      __ :'__" << endl;
+	cout << "   .'`__`-'__``." << endl;
+	cout << "  :__________.-'" << endl;
+	cout << "  :_________:" << endl;
+	cout << "   :_________`-;" << endl;
+	cout << "    `.__.-.__.'" << endl;
+
+	cout << endl;
+
+	cout << "      _.-'''''-._" << endl;
+	cout << "    .'  _     _  '." << endl;
+	cout << "   /   (_)   (_)   \\ " << endl;
+	cout << "  |  ,           ,  | " << endl;
+	cout << "  |  \\`.       .`/  | " << endl;
+	cout << "   \\  '.`'\"\"'\"`.'  /" << endl;
+	cout << "    '.  `'---'`  .'" << endl;
+	cout << "      '-._____.-'" << endl;
+
+	cout << endl;
+
+	cout << "   	 ,. " << endl;
+	cout << "    __.'_" << endl;
+	cout << "   |__|__|" << endl;
+	cout << "   |     | " << endl;
+	cout << "   |-___-|" << endl;
+	cout << "   '.___.'" << endl;
+
+	cout << endl;
+
+	cout << "       __..._   _...__" << endl;
+	cout << "  _..-\"      `Y`      \"-._" << endl;
+	cout << "  \\ Once upon |           /" << endl;
+	cout << "  \\\\  a time..|          //" << endl;
+	cout << "  \\\\\\         |         ///" << endl;
+	cout << "   \\\\\\ _..---.|.---.._ ///" << endl;
+	cout << "    \\\\`_..---.Y.---.._`//" << endl;
+	cout << "     '`               `'" << endl;
+
+	cout << endl;
+
+	cout << " ________________________" << endl;
+	cout << " |.----------------------.|" << endl;
+	cout << " ||                      ||" << endl;
+	cout << " ||                      ||" << endl;
+	cout << " ||     .-\"````\"-.       ||" << endl;
+	cout << " ||    /  _.._    `\\     ||" << endl;
+	cout << " ||   / /`    `-.   ; . .||" << endl;
+	cout << " ||   | |__  __  \\   |   ||" << endl;
+	cout << " ||.-.| | e`/e`  |   |   ||" << endl;
+	cout << " ||   | |  |     |   |'--||" << endl;
+	cout << " ||   | |  '-    |   |   ||" << endl;
+	cout << " ||   |  \\ --'  /|   |   ||" << endl;
+	cout << " ||   |   `;---'\\|   |   ||" << endl;
+	cout << " ||   |    |     |   |   ||" << endl;
+	cout << " ||   |  .-'     |   |   ||" << endl;
+	cout << " ||'--|/`        |   |--.||" << endl;
+	cout << " ||   ;    .     ;  _.\\  ||" << endl;
+	cout << " ||    `-.;_    /.-'     ||" << endl;
+	cout << " ||         ````         ||" << endl;
+	cout << " ||______________________||" << endl;
+	cout << " '------------------------'" << endl;
+
+	cout << endl;
+
+	cout << "      ; " << endl;
+	cout << "      ;;" << endl;
+	cout << "      ;';." << endl;
+	cout << "      ;  ;;" << endl;
+	cout << "      ;   ;;" << endl;
+	cout << "      ;    ;;" << endl;
+	cout << "      ;    ;;" << endl;
+	cout << "      ;   ;'" << endl;
+	cout << "      ;  ' " << endl;
+	cout << " ,;;;,; " << endl;
+	cout << " ;;;;;;" << endl;
+	cout << " `;;;;'" << endl;
+}
