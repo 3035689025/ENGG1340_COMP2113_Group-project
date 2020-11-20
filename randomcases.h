4f42f21980bb36a415d51d5332b719e4913d3c28
michaellian0422 <71887167+michaellian0422@users.noreply.github.com>
2020-11-21 07:15:22 +0800

    Update randomcases.h

--- randomcases.h
+++ randomcases.h
@@ -2,6 +2,6 @@
 #define RANDOMCASES_H
 #include "cases.h"
 
-void randomcases(int week, cases record[], int& salary, int& wrongattempts);
+void randomcases(int week, cases record[], int& salary, int& wrongattempts, int timecount);
 
 #endif
