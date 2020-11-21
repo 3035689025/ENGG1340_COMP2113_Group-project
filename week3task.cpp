efc60a35a727276f53b8e6d600e1fd1276080857
3035689025 <71936492+3035689025@users.noreply.github.com>
2020-11-21 18:39:45 +0800

    Update week3task.cpp
    added letter

--- week3task.cpp
+++ week3task.cpp
@@ -11,13 +11,39 @@ using namespace std;
 
 void week3task(int& week, int& money, int& wrongattempts, cases record[], int& salary)
 {
-	////// News
-	cout << "News: " << endl;
+	cout << "You: New week incoming... I have to focus on my work here first! No distractions!" << endl;
+	cout << "Press enter to continue" << endl;
+	cin.get();
+	system("clear");
 
-	///////Duty (Officer Kim)????
-	cout << "Your duty this week:" << endl << endl;
-	cout << "duty" << endl << endl;  /////////////////////////////////////////////////////////////////duty未填(外星人入得)
-	cout << "Press enter to start your work this week";
+	
+	//The focus will be on the rebels and plot (letter from mother) this week.
+	
+	//Your superior Officer Kim telling you your duties for this week
+	//icon of Officer Kim
+	cout << "                       ________________"            << endl;
+	cout << "                      \\      __      /         __"  << endl;
+	cout << "                       \\_____()_____/         /  )" << endl;
+	cout << "                       '============`        /  /"  << endl;
+	cout << "                        #---\\  /---#        /  /"   << endl;
+	cout << "                       (# @\\| |/@  #)      /  /"    << endl;
+	cout << "                        \\   (_)   /       /  /"     << endl;
+	cout << "                        |\\ '---` /|      /  /"      << endl;
+	cout << "                _______/ \\\\_____// \\____/ o_|"      << endl;
+	cout << "               /       \\  /     \\  /   / o_|"       << endl;
+	cout << "             /  |  _____     |       / /   \\ \\ "    << endl;
+	cout << "            /   |  |===|    o|      / /\\    \\ \\ "   << endl;  
+	cout << "           |    |   \\@/      |     / /  \\    \\ \\ "  << endl;
+	cout << "           |    |___________o|__/----)   \\    \\/ "  << endl;
+	cout << "           |    '              ||  --)    \\     |"  << endl;
+	cout << "           |___________________||  --)     \\    / " << endl;
+	cout << "                |           o|   ''''   |   \\__/ "  << endl;
+	cout << "                |            |          | "         << endl;
+	cout << endl << endl;
+	
+	cout << "Officer Kim: Your efficiency can be improved. Follow my instructions from last week and cross-check their passports and entry passes CAREFULLY!" << endl << endl;
+	cout << "Officer Kim: Be careful of the terrorist group, The Infected. There have been reports of them recruiting our officers. The punishment for treason is certain death." << endl << endl; 
+	cout << "Press enter to start your work this week" << endl;
 	cin.get();
 	system("clear");
 
@@ -61,14 +87,39 @@ void week3task(int& week, int& money, int& wrongattempts, cases record[], int& s
 	cout << "MMOMMMMOMMMOMMOOMMbT8bTSSSSSSSSSPd8OPdOOOMMMMOOMMMMOMMMOOMM" << endl;
 	cout << "MMOMMMOMMMMMOMMOOMMMbT8bTSSSSSPd88PdOOOOMMMMOOMMMMMMMMOOMMM" << endl;
 	
-	///////////////////////////////對白
+	//script betwen rebel and you, letter from mother here
 	cout << "\n \n";
-	cout << "You: You again?." << endl << endl;
-	cout << "Visitor: Read it and return to me immediately." << endl;
+	cout << "You: You again? I should arrest you." << endl << endl;
+	cout << "Visitor: I come in peace! Read it and destroy it immediately." << endl;
 	cout << "\n \n \n \n";
-	cout << "(A rebel,alien spy, introduces us to the existence of The Rebellion by handing us a pamphlet.)" << endl << endl; /////////////////////////////////////////////////////////not completed
-	cout << "Press enter to return the letter";
-	cin.get();
+	
+	//letter here
+	cout << "Dear Greg," << endl << endl;
+	
+	cout << "You may not believe me but I am your mother. First of all, I'm so sorry for leaving you here on Earth. It must have been tough." << endl;
+	cout << "But...I had my reasons. You are actually from a bloodline of Arstotzkans. You may know us as The infected, The Terrorists or whatever humans call us now. ";
+	cout << "I left you there for a better chance of survival. We were hunted and persecuted for centuries. I didn't want you to live like us.";
+	cout << "But now we are fighting back, and we're actually winning! I want you to join us to fight for OUR people." << endl << endl;
+	
+	cout << "You must be confused right now, but understand this: everything you know is a lie. 300 years ago, Earth was occupied by the \"alien\" species, ";
+	cout << "The Arstotzkans. There were no humans. But one day...humans came to our planet looking for resources and a place to survive. It was perfect. ";
+	cout << "Earth was filled with food and living beings. Humans drove nearly all of us out of our land by the 1800s. As victory is written by the victors, ";
+	cout << "history books have referred to the deportation of \"The Infected species\" as a victory - humans defending their motherland against aliens. ";
+	cout << "The hypocrisy! Even the name \"The Infected\" was to associate us with a negative stigma to alienate us from Earth. Soon, humans will drain all the resources of ";
+	cout << "this beautiful planet and move on to the next one. " << endl << endl;
+	
+	cout << "Join us and fight for your people!" << endl << endl;
+	cout << "p.s. The name Greg was given by me to remind you to be vigilant. Remember, always stay vigilant." << endl << endl;
+	cout << "Much love, \nMom \n \n \n \n ";
 
+	cout << "Press enter to burn the letter";
+	cin.get();
+	system("clear");
+	
+	
+	cout << "You: Wow...that's a lot to take in..." << endl << endl;
+	cout << "Press enter to continue";
+	cin.get();
+	system("clear");
 
 }
