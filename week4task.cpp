401a8b02967300faf8ddcebd35856a63f8ddd48d
3035689025 <71936492+3035689025@users.noreply.github.com>
2020-11-21 14:20:15 +0800

    Update week4task.cpp
    added news and instructions for week 4

--- week4task.cpp
+++ week4task.cpp
@@ -13,25 +13,52 @@ using namespace std;
 void week4task(int& week, int& money, int& wrongattempts, cases record[], int& salary)
 {
 
-	////// News or 
+	//news talking about The Infected
 	cout << "News: " << endl;
-	cout << "Press enter to start your work this week" << endl;
+	cout << "___________________________________________________________________" << endl << endl;
+	cout << "An explosion happened in Venice, Italy yesterday. There were 10 deaths and 28 injured in this horrific attack. The terrorist group, The Infected, ";
+	cout << "has claimed responsibility for this attack." << endl << endl;
+	cout << "Press enter to continue" << endl;
 	cin.get();
 	system("clear");
+	
 
+	//Officer Kim 
+	cout << "                       ________________"            << endl;
+	cout << "                      \\      __      /         __"  << endl;
+	cout << "                       \\_____()_____/         /  )" << endl;
+	cout << "                       '============`        /  /"  << endl;
+	cout << "                        #---\\  /---#        /  /"   << endl;
+	cout << "                       (# @\\| |/@  #)      /  /"    << endl;
+	cout << "                        \\   (_)   /       /  /"     << endl;
+	cout << "                        |\\ '---` /|      /  /"      << endl;
+	cout << "                _______/ \\\\_____// \\____/ o_|"      << endl;
+	cout << "               /       \\  /     \\  /   / o_|"       << endl;
+	cout << "             /  |  _____     |       / /   \\ \\ "    << endl;
+	cout << "            /   |  |===|    o|      / /\\    \\ \\ "   << endl;  
+	cout << "           |    |   \\@/      |     / /  \\    \\ \\ "  << endl;
+	cout << "           |    |___________o|__/----)   \\    \\/ "  << endl;
+	cout << "           |    '              ||  --)    \\     |"  << endl;
+	cout << "           |___________________||  --)     \\    / " << endl;
+	cout << "                |           o|   ''''   |   \\__/ "  << endl;
+	cout << "                |            |          | "         << endl;
+	cout << endl << endl;
+	cout << "Officer Kim: Approve humans and aliens with valid passports and entry passes. " << endl;
+	cout << "Now take a look at this wanted list. They are members of The Infected. Do NOT allow them to enter. Remember the names.";
+	cout << "Since this list is highly classified, you will NOT be able to read them again." << endl << endl;
+	cout << "Press enter to read the wanted list" << endl;
+	cin.get();
+	system("clear");
+	
+	
 	////// The Wanted list
 	cout << "The wanted list" << endl;
+	cout << "___________________________________________________________________" << endl << endl;
+	cout << "Eagle Flies \n Patricia Aniston \n Thomas Carter Jr. \n" << endl;
 	cout << "Press enter to start your work this week" << endl;
 	cin.get();
 	system("clear");
-
-
-	///////Duty (Officer Kim)????
-	cout << "Your duty this week:" << endl << endl;
-	cout << "duty" << endl << endl;  /////////////////////////////////////////////////////////////////duty未填(外星人入得)
-	cout << "Press enter to start your work this week" << endl;
-	cin.get();
-	system("clear");
+	
 
 	// For indicate the occurance of the visotr on wanted list.
 	int wantedlist1 = 0;
